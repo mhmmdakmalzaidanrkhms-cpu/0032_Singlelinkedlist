f16ab89c92f6c62b52bd8819a6c34bb781af2356
zidan <mhmmdakmalzaidanrkkms@gmail.com>
2026-04-23 21:06:21 +0800

    commit 4

--- 0032_singlelinkedlist.cpp
+++ 0032_singlelinkedlist.cpp
@@ -43,8 +43,6 @@ if (START == NULL || nim <= START->noMhs)
         return;
      }
 
-     // commit 3
-
      Node *previous =START;
      Node *current = START;
 
@@ -81,6 +79,8 @@ return;
 
         return (current != NULL);
     }
+
+    // commit 4
 
 
     bool delNode(int nim)
